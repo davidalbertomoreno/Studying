7caeabcd90aba89826b495fc4fb56d8a0667a8c9
davidalbertomoreno <davidalbertomoreno@github.com>
2018-11-17 12:35:52 -0300

    Sábado 17.

--- 2018/Noviembre/Experimentos/Ordenador_Cifras.java
+++ 2018/Noviembre/Experimentos/Ordenador_Cifras.java
@@ -8,17 +8,20 @@ public class Ordenador_Cifras {
 		
 Scanner scan=new Scanner(System.in);
 System.out.println("Introduzca números para ordenarlos de manera ascendente (por ejemplo: «2, 8, 3, 6, 7, 3»):\n");
-
-int datosIntroducidos=scan.nextInt();
-
-int[]array=new int[1000];
-if(datosIntroducidos!=0){
-            do{
-               for (int i=0; i<array.length; i++){
- 
-                  array[i]=datosIntroducidos; }
-	}
-
+int números=scan.nextInt();
+int[] array = new int[1000];
+if(números==0){
+	System.out.println(+números);
+	{
+		};
 }
+          //  do{
+               //for (int i=0; i<array.length; i++){
+ 
+                  //array[i]=datosIntroducidos; }
+        //       while
+	
+}
+	}
 
 //int numeros[] = new int[] {8,7,34,6,2,6,86,45,35,75,3,2,6,8,9,35,46,2,46,57};
