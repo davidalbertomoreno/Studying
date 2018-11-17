3167f82dd50195e50a3ce3bdc516d6b6db222a5f
davidalbertomoreno <davidalbertomoreno@github.com>
2018-11-17 12:39:06 -0300

    Sábado 17.

--- 2018/Noviembre/Experimentos/Ordenador_Cifras.java
+++ 2018/Noviembre/Experimentos/Ordenador_Cifras.java
@@ -19,7 +19,7 @@ if(números==0){
                //for (int i=0; i<array.length; i++){
  
                   //array[i]=datosIntroducidos; }
-        //       while
+        //       while    a
 	
 }
 	}
