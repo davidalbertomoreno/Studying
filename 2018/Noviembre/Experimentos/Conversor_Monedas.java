516503fa97311cf9ea1d9bf005ef4bf871ba4de0
davidalbertomoreno <45082246+davidalbertomoreno@users.noreply.github.com>
2018-11-23 21:42:14 -0300

    Update Conversor_Monedas.java

--- 2018/Noviembre/Experimentos/Conversor_Monedas.java
+++ 2018/Noviembre/Experimentos/Conversor_Monedas.java
@@ -1,22 +1,22 @@
 package Experimentos;
 import java.util.Scanner;
 public class Conversor_Monedas {
+	private static Scanner sc;
 	public static void main(String[] args) {
 		
-		float cantidad;
-		float total = 0;
-		System.out.println("Ingrese tipo de moneda: \n\n1. USD\n2. EUR\n");
-		Scanner scanner = new Scanner(System.in);
-		Scanner sc = new Scanner(System.in);
-		String inputString = scanner.nextLine();
-		System.out.println("\nIngrese la cantidad en "+inputString+" para convertir a CLP:\n");
-		cantidad = sc.nextFloat();
-		if(inputString.equalsIgnoreCase("USD")){
-			total = cantidad*688;
-		System.out.println("\nLa conversiÛn resultante es $"+total+".");
-		}else if(inputString.equalsIgnoreCase("EUR")){
-			total = cantidad*722.12f;
-			System.out.println("\nLa conversiÛn resultante es: $"+total+".");
+		float Cantidad;
+		float Total = 0;
+		System.out.println("Ingrese divisa: \n\n1. USD\n2. EUR\n");
+		sc = new Scanner(System.in);
+		String Divisa = sc.nextLine();
+		System.out.println("\nIngrese cantidad en "+Divisa+" para su conversi√≥n en pesos chilenos:\n");
+		Cantidad = sc.nextFloat();
+		if(Divisa.equalsIgnoreCase("USD")){
+			Total = Cantidad*675;
+		System.out.println("\nSu conversi√≥n a CLP resultante es: $"+Total+" pesos chilenos.");
+		}else if(Divisa.equalsIgnoreCase("EUR")){
+			Total = Cantidad*765;
+			System.out.println("\nSu conversi√≥n a CLP resultante es: $"+Total+" pesos chilenos.");
 		}
 	}
 }
